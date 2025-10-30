347c5aa263c88b90a559c99d50a57b192a42dbb6
Fonte <lfonte007@gmail.com>
2025-10-30 19:34:41 -0300

    feat: get project by uuid

--- app/src/Project/index.ts
+++ app/src/Project/index.ts
@@ -1,12 +1,19 @@
 import { CreateProjectController } from './Controllers/CreateProjectController';
+import { GetProjectByUuidController } from './Controllers/GetProjectByUuidController';
 import { ProjectStorage } from './Repositories/ProjectStorage';
 import { CreateProjectService } from './Services/CreateProjectService';
+import { GetProjectByUuidService } from './Services/GetProjectByUuidService';
 import { CreateProjectTransformer } from './Transformers/CreateProjectTransformer';
+import { GetProjectByUuidTransformer } from './Transformers/GetProjectByUuidTransformer';
 
 const storage = new ProjectStorage();
 
+// CREATE PROJECT EXPORTS
 const createProjectTransformer = new CreateProjectTransformer();
-
 const createProjectService = new CreateProjectService(createProjectTransformer, storage);
-
 export const createProjectController = new CreateProjectController(createProjectTransformer, createProjectService);
+
+// GET PROJECT BY UUID EXPORTS
+const getProjectByUuidTransformer = new GetProjectByUuidTransformer();
+const getProjectByUuidService = new GetProjectByUuidService(getProjectByUuidTransformer, storage);
+export const getProjectByUuidController = new GetProjectByUuidController(getProjectByUuidTransformer, getProjectByUuidService);
